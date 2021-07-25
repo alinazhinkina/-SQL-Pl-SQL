8bd8f59aa8ab4b515b4db33bb30cba99f98f39d4
alinazhinkina <zhinkinaalina@gmail.com>
2021-07-25 21:45:10 +0300

    Delete lesson_4.sql

--- lesson_4.sql
+++
@@ -1,9 +0,0 @@
---создаем синонимы к таблице goods схемы username
-create synonym user_goods for username.goods;
-select * from user_goods
-
---публичный синоним, доступ будет и у других пользователей
-create public synonym user_goods_public for username.goods;
-
---команда для загрузки данных из файла с помощью SQL*Loader
---sqlldr username/111 control=D:\Database\Oracle\scripts\insert_employees.ctl data=D:\Database\Oracle\scripts\insert_employees.csv
